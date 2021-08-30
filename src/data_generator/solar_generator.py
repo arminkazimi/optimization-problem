56cb9f0ab102de89738230b20eb6d669b708157f
arminkazimi <arminkazimi@gmail.com>
2021-08-31 02:26:18 +0430

    1 hour resolution

--- src/data_generator/solar_generator.py
+++ src/data_generator/solar_generator.py
@@ -6,15 +6,19 @@ from datetime import datetime, timedelta, time, date
 
 class SolarGenerator:
     def __init__(self, from_date, to_date, lat, long, resolution):
-        self.from_date = datetime.strptime(from_date, '%Y-%m-%d')
-        self.to_date = datetime.strptime(to_date, '%Y-%m-%d')
+        self.from_date = datetime.strptime(from_date, '%Y-%m-%d %H:%M:%S')
+        self.to_date = datetime.strptime(to_date, '%Y-%m-%d %H:%M:%S')
         self.lat = lat
         self.long = long
         self.resolution = resolution
 
     def generator(self):
         city = LocationInfo(latitude=self.lat, longitude=self.long)
-        delta = timedelta(days=1)
+        if self.resolution == '1hour':
+            delta = timedelta(hours=1)
+
+        if self.resolution == '1day':
+            delta = timedelta(days=1)
 
         df = pd.DataFrame(columns=['datetime', 'sunrise', 'noon', 'sunset'])
         datetime_list = list()
@@ -24,7 +28,7 @@ class SolarGenerator:
         while self.from_date <= self.to_date:
             s = sun(city.observer,
                     date=date(self.from_date.year, self.from_date.month, self.from_date.day))
-            datetime_list.append(self.from_date.strftime('%Y-%m-%d'))
+            datetime_list.append(self.from_date.strftime('%Y-%m-%d %H:%M:%S'))
             sunrise_list.append(s['sunrise'])
             noon_list.append(s['noon'])
             sunset_list.append(s['sunset'])
--- src/data_generator/wind_generator.py
+++ src/data_generator/wind_generator.py
@@ -1,18 +1,20 @@
-from meteostat import Point, Daily
+from meteostat import Point, Daily, Hourly
 from datetime import datetime, timedelta, time, date
 
 
 class WindGenerator:
     def __init__(self, from_date, to_date, lat, long, resolution):
-        self.from_date = datetime.strptime(from_date, '%Y-%m-%d')
-        self.to_date = datetime.strptime(to_date, '%Y-%m-%d')
+        self.from_date = datetime.strptime(from_date, '%Y-%m-%d %H:%M:%S')
+        self.to_date = datetime.strptime(to_date, '%Y-%m-%d %H:%M:%S')
         self.lat = lat
         self.long = long
         self.resolution = resolution
 
     def generator(self):
         location_point = Point(lat=self.lat, lon=self.long)
-
-        data = Daily(location_point, self.from_date, self.to_date)
+        if self.resolution == '1hour':
+            data = Hourly(location_point, self.from_date, self.to_date)
+        if self.resolution == '1day':
+            data = Daily(location_point, self.from_date, self.to_date)
         data = data.fetch()
         return data
